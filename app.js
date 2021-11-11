d961c053f30ad073665eca4031e2fca0e52ada38
PonceGL <poncianogl@hotmail.com>
2021-11-10 18:02:00 -0600

    actualizacion del puerto

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Imports
 const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const port = 3000;
+//settings
+app.set("port", process.env.PORT || 3000);
 
 // Static Files
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 
 // Set Templating Engine
-// app.use(expressLayouts);
-// app.set("layout", "./layouts/full-width");
 app.set("view engine", "ejs");
 
 // Routes
 app.use(require("./routes/index"));
 
-// Listen on Port 5000
-app.listen(port, () => console.info(`App listening on port ${port}`));
+// Starting the server
+app.listen(app.get("port"), () => {
+  console.log(`Aplicación escuchando en http://localhost:${app.get("port")}`);
+});
